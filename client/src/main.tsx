8a8287a045ce3825f955c6270f4249ccbafa6153
Marco Araya Jiménez <marcojime23@gmail.com>
2025-05-31 12:06:16 -0600

    feat: add PostHog integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,28 +1,74 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
+import { posthogInstance } from "./context/PosthogConfig";
+import { PostHogProvider } from 'posthog-js/react';
+
+// Dojo & Starknet
+import { init } from "@dojoengine/sdk";
+import { DojoSdkProvider } from "@dojoengine/sdk/react";
+import { dojoConfig } from "./dojo/dojoConfig";
+import type { SchemaType } from "./dojo/bindings";
+import { setupWorld } from "./dojo/contracts.gen";
+import StarknetProvider from "./dojo/starknet-provider";
+
+// App Entry
+import Main from "../src/app/App";
+
+// Styles
 import "./global.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 // PWA Service Worker
 if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker
-            .register("/sw.js", { type: "module" })
-            .then((registration) => {
-                console.log("ServiceWorker registration successful:", registration);
-            })
-            .catch((error) => {
-                console.log("ServiceWorker registration failed:", error);
-            });
-    });
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js", { type: "module" })
+      .then((registration) => {
+        console.log("ServiceWorker registration successful:", registration);
+      })
+      .catch((error) => {
+        console.log("ServiceWorker registration failed:", error);
+      });
+  });
 }
 
-function main() {
-    createRoot(document.getElementById("root")!).render(
-        <StrictMode>
-            <App />
-        </StrictMode>
-    );
+// Init Dojo
+async function main() {
+  const sdk = await init<SchemaType>({
+    client: {
+      toriiUrl: dojoConfig.toriiUrl,
+      relayUrl: dojoConfig.relayUrl,
+      worldAddress: dojoConfig.manifest.world.address,
+    },
+    domain: {
+      name: "ByteBeasts Tamagotchi",
+      version: "1.0",
+      chainId: "KATANA",
+      revision: "1",
+    },
+  });
+
+  const rootElement = document.getElementById("root");
+  if (!rootElement) throw new Error("Root element not found");
+
+  createRoot(rootElement).render(
+    <StrictMode>
+        <DojoSdkProvider sdk={sdk} dojoConfig={dojoConfig} clientFn={setupWorld}>
+          <StarknetProvider>
+          {posthogInstance.initialized && posthogInstance.client ? (
+              <PostHogProvider client={posthogInstance.client}>
+                <Main />
+              </PostHogProvider>
+            ) : (
+              <Main />
+            )}
+          </StarknetProvider>
+        </DojoSdkProvider>
+    </StrictMode>
+  );
 }
 
-main();
+main().catch((error) => {
+  console.error("Failed to initialize the application:", error);
+});
