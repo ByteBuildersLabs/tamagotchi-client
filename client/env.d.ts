cf1e8e82e63bb76b8dbb20a978b24ba1cad631c9
Marco Araya Jiménez <75222804+coxmars@users.noreply.github.com>
2025-05-31 12:39:18 -0600

    feat: add init configs for controller and dynamic deployment for Sepolia/Mainnet (#33)

---
+++ client/env.d.ts
@@ -0,0 +1,11 @@
+interface ImportMetaEnv {
+    readonly VITE_PUBLIC_DEPLOY_TYPE: "mainnet" | "sepolia";
+    readonly VITE_PUBLIC_NODE_URL: string;
+    readonly VITE_PUBLIC_TORII: string;
+    readonly VITE_PUBLIC_MASTER_ADDRESS?: string;
+    readonly VITE_PUBLIC_MASTER_PRIVATE_KEY?: string;
+}
+  
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
